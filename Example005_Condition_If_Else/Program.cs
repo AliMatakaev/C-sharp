75342de6d191c468091fe5a42574a8f756dd39b8
Ali <ali13-09@mail.ru>
2023-09-14 01:19:00 +0300

    --

---
+++ Example005_Condition_If_Else/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите ваше имя: ");
+string username = Console.ReadLine();
+Console.WriteLine(username);
+if (username == "Маша")
+{
+    Console.WriteLine("Ура, это же МАША!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.Write(username);
+}
