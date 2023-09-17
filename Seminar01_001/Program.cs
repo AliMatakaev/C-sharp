98183c622cf7c95055c4ab2e0203300f0e8a778d
Ali <ali13-09@mail.ru>
2023-09-17 22:54:00 +0300

    add files

---
+++ Seminar01_001/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Введите трехзначное число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+if (n > 99 && n < 1000)
+{
+    Console.WriteLine(n % 10);
+}
+else Console.WriteLine("Некорректный ввод");
