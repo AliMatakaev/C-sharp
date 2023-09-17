df46465ab2ce4724a7dbd922d415eabfb8560e5c
Ali <ali13-09@mail.ru>
2023-09-17 23:37:45 +0300

    Seminar 1, Task 8

---
+++ Seminar_1_Task_8/Program.cs
@@ -0,0 +1,9 @@
+﻿// Написать программу, которая на вход принимает число N, и выдает все четные числа от 1 до N.
+Console.WriteLine("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int result = 2;
+while (result <= n)
+{
+    Console.Write($"{result}, ");
+    result = result + 2;
+}
