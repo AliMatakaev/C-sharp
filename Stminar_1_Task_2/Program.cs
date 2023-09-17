835c6da2a95d2c61f5879df1824aaf82ee97d807
Ali <ali13-09@mail.ru>
2023-09-17 22:39:22 +0300

    Seminar 1, Task 2

---
+++ Stminar_1_Task_2/Program.cs
@@ -0,0 +1,11 @@
+﻿// Написать программу, которая принимает на вход два числа, 
+// и выдает, какое число больше, а какое меньше
+
+Console.WriteLine("Введите два числа: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int m = Convert.ToInt32(Console.ReadLine());
+if (m > n)
+{
+    Console.WriteLine($"Число {m} больше, чем число {n}");
+}
+else Console.WriteLine($"Число {n} больше, чем число {m}");
