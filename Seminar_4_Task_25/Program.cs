30152fbe975b52a02ad7f8474586be273df2d4c3
Ali <ali13-09@mail.ru>
2023-10-03 00:44:52 +0300

    Task 25

---
+++ Seminar_4_Task_25/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите число A: ");
+int A = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите число B: ");
+int B = Convert.ToInt32(Console.ReadLine());
+if (B == 0) System.Console.WriteLine(1);
+else
+{
+    int result = A;
+for (int i = 1; i < B; i++)
+{
+   result = result * A; 
+}
+System.Console.WriteLine(result);
+}
